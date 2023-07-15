7b522d9f856c292d0bdeba2dba37513e4c10902a
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-15 17:20:42 -0500

    💼 (46:09) Dev SideBar

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from 'react';
+import SideBar from './SideBar';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }} >
+        <SideBar />
+        <Typography className="copyriht" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+          Copyleft 🄯 2023 YouTube Clone
+        </Typography>
+      </Box>
+    </Stack >
   )
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,12 +3,14 @@ import Feed from './Feed';
 import Navbar from './Navbar';
 import SearchFeed from './SearchFeed';
 import VideoDetail from './VideoDetail';
+import SideBar from './SideBar';
 
 export {
   ChannelDetail,
   Feed,
   Navbar,
   SearchFeed,
-  VideoDetail
+  VideoDetail,
+  SideBar,
 };
 
