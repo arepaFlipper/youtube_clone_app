062981a57dd35e9ef5f86a8f118fbf9ff39e9618
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-15 11:23:22 -0500

    📍 (24:00) include components

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+import ChannelDetail from './ChannelDetail';
+import Feed from './Feed';
+import Navbar from './Navbar';
+import SearchFeed from './SearchFeed';
+import VideoDetail from './VideoDetail';
+
+export {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail
+};
+
