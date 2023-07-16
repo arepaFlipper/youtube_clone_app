e86b6d45517fe359eb99f0f23b02fa1e578d9a1d
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-16 13:39:42 -0500

    🔎 fix the url

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -7,7 +7,7 @@ const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState('New');
   const [videos, setVideos] = useState([]);
   useEffect(() => {
-    fetchFromAPI(`/search?q=${selectedCategory}`)
+    fetchFromAPI(`/search?part=snippet&q=${selectedCategory}`)
       .then((data) => {
         setVideos(data.items);
       });
