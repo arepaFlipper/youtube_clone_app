fcfa78f733d9e31f0d7ec2fe4e43a70832fefa75
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-16 15:43:51 -0500

    🥨 (1:20:14) display VideoCard w/ right size

--- src/components/ChannelCard.tsx
+++ src/components/ChannelCard.tsx
@@ -1,12 +1,22 @@
 import { ItemProps } from '../types';
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
 
 interface IProps {
   channel: ItemProps;
 }
 
+// @ts-ignore
 const ChannelCard = ({ channel }: IProps) => {
+  const { id, snippet } = channel;
+  const { channelId } = id;
   return (
-    <div style={{ color: 'white' }}>ChannelCard</div>
+    <Card>
+      <Link to={channelId ? `/video/${channelId}` : demoVideoUrl}>
+        <CardMedia image={snippet?.thumbnails?.high?.url} alt={snippet?.title} sx={{ width: '358px', height: '150px' }} />
+      </Link>
+    </Card >
   )
 }
 
--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -13,10 +13,23 @@ const VideoCard = ({ video }: IProps) => {
   const { id, snippet } = video;
   const { videoId } = id;
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 'none' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia image={snippet?.thumbnails?.high?.url} alt={snippet?.title} sx={{ width: '358px', height: '150px' }} />
       </Link>
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link to={(snippet?.channelId) ? `/channel/${snippet.channelId}` : demoVideoUrl}>
+          <Typography variant="subtitle2" color="#878787">
+            {snippet?.channelTitle.slice(0, 60) || demoChannelTitle.slice(0, 60)}
+            <CheckCircle sx={{ fontSize: '12px', color: '#1AA7EC', ml: '6px' }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card >
   )
 }
