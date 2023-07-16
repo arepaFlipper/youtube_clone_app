065e08b9e687872b8e79e1bd45b870e9cbacb87c
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-15 20:19:21 -0500

    🪘 I figured these Generic types out

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,4 +1,14 @@
-const Videos = ({ videos }) => {
+type VideosProps<T> = {
+  videos: T[]
+}
+type IVideo = {
+  kind: string;
+  id: {
+    kind: string;
+    videoId: string;
+  }
+}
+const Videos = ({ videos }: VideosProps<IVideo[]>) => {
   return (
     <div>Videos</div>
   )
